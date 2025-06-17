46698ac40a7fcfb2dd7ec6fd513b08301441598e
Yonathan <111770436+YonaAncheo@users.noreply.github.com>
2025-06-17 17:47:05 -0400

    Agregué la función para mostrar la información al presionar cada botón, por lo que ahora aprece la información por defecto, luego se puede navegar entre calculadora, minijuego e información. Calculadora realiza la función correctamente al completar el formulario, pero el minijuego no muestra los elementos fuera del canvas, estoy debuggeando esto.

--- EcoConcienciaApp/www/js/main.js
+++ EcoConcienciaApp/www/js/main.js
@@ -6,3 +6,21 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
+
+
+function mostrarSeccion(id) {
+  const secciones = document.querySelectorAll("section");
+  secciones.forEach(seccion => {
+    seccion.style.display = "none";
+  });
+
+  const seccionActiva = document.getElementById(id);
+  if (seccionActiva) {
+    seccionActiva.style.display = "block";
+  }
+}
+
+// Mostrar la primera sección por defecto (información)
+document.addEventListener("DOMContentLoaded", () => {
+  mostrarSeccion("informacion");
+});
